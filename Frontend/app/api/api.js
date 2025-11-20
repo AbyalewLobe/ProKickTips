79e671891ff0120365acd534a760377cf60c3f22
AbyalewLobe <labyalew@gmail.com>
2025-11-20 11:09:54 -0800

    add frontend url fallback

--- Frontend/app/api/api.js
+++ Frontend/app/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_URL ||
+    "https://pro-kick-tips-qzee8qrrf-abyalews-projects.vercel.app/api",
   withCredentials: true,
 });
 
